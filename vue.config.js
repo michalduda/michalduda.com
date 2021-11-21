4742d818a8c697a0b060d5fc08ec87a143b3c6e5
Michał Duda <this.michalduda@gmail.com>
2021-11-21 19:42:14 +0100

    Added scss helpers

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,26 @@
+const path = require('path')
+
+const addStyleResource = rule => {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/styles/helpers.scss')]
+    })
+}
+
 module.exports = {
+  productionSourceMap: false,
   pluginOptions: {
     windicss: {
       // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
     }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  chainWebpack (config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
   }
 }
