a084bd811142f5b5507527b3e59e42393f76075a
Michał Duda <this.michalduda@gmail.com>
2021-11-30 23:52:01 +0100

    Adjusted windi config

---
+++ windi.config.js
@@ -0,0 +1,18 @@
+// import colors from 'windicss/colors'
+
+export default {
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px'
+    },
+    fontFamily: {
+      primary: ['Karla', 'sans-serif'],
+      secondary: ['Space Grotesk', 'serif']
+    }
+  }
+}
