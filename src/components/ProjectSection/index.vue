b3d113cbcb20c54514150e1f68ee770c487c2aea
Michał Duda <this.michalduda@gmail.com>
2021-12-12 21:39:11 +0100

    Reordered components in projects on mobile

--- src/components/ProjectSection/index.vue
+++ src/components/ProjectSection/index.vue
@@ -1,27 +1,31 @@
 <template>
-  <section class="projects-section section grid border-b border-t border-black">
-    <!-- <SectionHeader class="col-span-12 mt-4">
-      Selected projects
-    </SectionHeader> -->
-
-    <ProjectDetails
-      class="col-span-12 mt-4
-        sm:col-span-6 sm:row-start-2 sm:my-8
-        lg:col-span-5"
+  <section
+    class="projects-section section grid border-b border-black
+      sm:border-t"
+  >
+    <SectionHeader
+      class="col-span-12 mt-4"
+      text="Selected projects"
+      v-if="!breakpoints.sm"
     />
 
-    <div
-      class="projects-section__buttons-container col-span-12 stretch-to-edge"
-      v-if="!breakpoints.sm"
-    >
-      <SlideControls class="mb-4"/>
-    </div>
     <div
       class="col-span-12 stretch-to-edge overflow-hidden
         sm:col-span-5 sm:row-start-2 sm:col-start-8
         lg:col-span-6 lg:col-start-7"
     >
       <ProjectVisual />
+    </div>
+    <ProjectDetails
+      class="col-span-12 mt-4
+        sm:col-span-6 sm:row-start-2 sm:my-8
+        lg:col-span-5"
+    />
+    <div
+      class="projects-section__buttons-container col-span-12 stretch-to-edge"
+      v-if="!breakpoints.sm"
+    >
+      <SlideControls class="mb-4"/>
     </div>
   </section>
 </template>
@@ -30,6 +34,7 @@
 import ProjectDetails from '@/components/ProjectSection/ProjectDetails'
 import ProjectVisual from '@/components/ProjectSection/ProjectVisual'
 import SlideControls from '@/components/ProjectSection/SlideControls'
+import SectionHeader from '@/components/SectionHeader'
 import { mapState } from 'pinia'
 import { screenStore } from '@/store/screen'
 
@@ -37,7 +42,8 @@ export default {
   components: {
     ProjectDetails,
     ProjectVisual,
-    SlideControls
+    SlideControls,
+    SectionHeader
   },
   computed: {
     ...mapState(screenStore, ['breakpoints'])
